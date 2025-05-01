25ea772cc0c05d07da8191e21bb0ccfd842936a1
Rodrigo <miocidcampeador1@hotmail.com>
2025-05-01 12:36:04 -0600

    destruir animales fuera de pantalla
    se codifico para que los animales desaparescan al salir de la pantalla

--- Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBounnd = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,10 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound) {
+        if (transform.position.z > topBound) {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBounnd )
+        {
             Destroy(gameObject);
         }
     }
