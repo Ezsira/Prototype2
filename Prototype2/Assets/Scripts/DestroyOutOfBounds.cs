42440aa0d27c189ae19126b5a1dd879238ed8a99
Rodrigo <miocidcampeador1@hotmail.com>
2025-05-01 14:08:53 -0600

    Proyecto completado
    se completo el proyecto al incluir colisiones para enemigos y municion, y hacer los spawns de los enemigos aleatorias

--- Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -9,15 +9,17 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound) {
+        if (transform.position.z > topBound)
+        {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBounnd )
+        }
+        else if (transform.position.z < lowerBounnd)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
--- Prototype2/Assets/Scripts/SpawnManger.cs
+++ Prototype2/Assets/Scripts/SpawnManger.cs
@@ -12,18 +12,14 @@ public class SpawnManger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal",startDelay,spawnInterval);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
         {
-          
-            
         }
-        
     }
     void SpawnRandomAnimal()
     {
